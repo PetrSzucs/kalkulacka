a30daa14e712a461248eea950c2e9a74b7194c59
PetrSzucs <50320680+PetrSzucs@users.noreply.github.com>
2022-06-10 18:50:37 +0200

    Prvni commit

---
+++ kalkulacka.py
@@ -0,0 +1,16 @@
+class kalkulacka:
+	@classmethod
+	def secti(cls,a,b):
+		return a+b
+
+	@classmethod
+	def odecti(cls,a,b):
+		return a-b
+
+def main():
+	kalkulajda= kalkulacka()
+	print(kalkulacka.secti(5,4))
+	print(kalkulacka.odecti(5,4))
+
+if __name__== "__main__":
+	main()
